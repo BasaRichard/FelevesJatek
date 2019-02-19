d1be2d160396cd5484efe087aa06565fd768012e
Basa Richard <ricsi3117@gmail.com>
2019-02-19 10:23:41 +0100

    random comment

--- FelevesJatek/Program.cs
+++ FelevesJatek/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace FelevesJatek
 {
+    // PDF : http://users.nik.uni-obuda.hu/siposm/lectures/SZTF2/LA-01/prog2-feleves-jatek-gyakfeladat.pdf
     class Program
     {
         static void Main(string[] args)
